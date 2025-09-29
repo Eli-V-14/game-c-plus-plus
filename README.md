dbfe5dcab3882057d86128369cf6b2c9469f6eaf
vanget <vanget@msoe.edu>
2025-09-28 23:18:53 -0500

    Added some more descriptions into README.md and added initial movement for user.

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # paint-ball
-Making a Paint Ball multiplayer game for a term project in C++
+Making a Paint Ball multiplayer game for a Term project in C++
+
+Initial Game Concept:
+- Singular box (User) with a paintball gun
+- Different modes
+  - Zombie
+    - Destroy Zombies
+    - Collect EXP/Points
+    - Weapon Upgrades
+    
+  - 2 Person / Multiplayer
+    - UDP Streams
+    - Capture the Flag
+    - Free for All
+
+
+Sources of Information:
+- https://www.freecodecamp.org/news/c-game-development-with-raylib/
+- https://www.youtube.com/watch?v=Xp-zn1TyMjU&t=305s
--- game.cpp
+++ game.cpp
@@ -1,7 +1,51 @@
 //
-// Created by vanget on 9/26/2025.
+// Created by Eli Vang on 9/26/2025.
 //
 
+#include <raylib.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+#include <math.h>
+
+void update_player(int &x, int &y) {
+    int normalized = sqrt(50) / 5;
+    if (IsKeyDown(KEY_UP) && (IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_RIGHT))) {
+        y -= normalized;
+        x -= normalized;
+    }
+
+
+    if (IsKeyDown(KEY_UP)) y -= 5;
+    if (IsKeyDown(KEY_DOWN)) y += 5;
+    if (IsKeyDown(KEY_LEFT)) x -= 5;
+    if (IsKeyDown(KEY_RIGHT)) x += 5;
+}
+
 int main() {
+    constexpr int screen_width = 800;
+    constexpr int screen_height = 800;
+
+    int player_dim = 30;
+    int player_x = screen_width/2 - player_dim/2;
+    int player_y = screen_height/2 - player_dim/2;
+
+    InitWindow(screen_width, screen_height, "Paint Ball Game");
+    SetTargetFPS(60);
+    srand(time(NULL));
+
+    while (!WindowShouldClose()) {
+        // update game logic should go here
+
+        BeginDrawing();
+        ClearBackground(WHITE);
+        // everything we want to draw has to be done within Begin/End Drawing
+        update_player(player_x, player_y);
+
+        DrawRectangle(player_x, player_y, player_dim, player_dim, BLUE);
+
+        EndDrawing();
+    }
+    CloseWindow();
     return 0;
 }
